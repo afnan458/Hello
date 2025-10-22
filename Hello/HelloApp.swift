140b1efaccfa91c38a4dbcb5543e6cadb04d098e
Afnan <ahassan25@twq.idesrve.net>
2025-10-22 14:27:58 +0300

    Initial Commit

---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Afnan hassan on 30/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
